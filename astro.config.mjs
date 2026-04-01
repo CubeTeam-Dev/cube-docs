89374c816c539a640894f1f94624bfb7781b247a
tah5882 <me@tah5882.dev>
2026-04-01 23:54:29 +0900

    feat: implement custom theme, update site configuration, and refresh favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Cube Bot Docs",
+      logo: {
+        src: "./public/favicon.svg",
+      },
+      social: [
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/BynQSCJTeg",
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "はじめに",
+          items: [{ label: "Cube Bot について", link: "/" }],
+        },
+      ],
+    }),
+  ],
 });
